4afc62dcd3cfa0da872d91cae1eb464669feb228
mhowe <michael.howe25@gmail.com>
2019-08-14 21:00:05 -0600

    add image to about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="about">
     <h1>Who made this?</h1>
+    <img src="../assets/images/splash.jpg" alt="">
+    <h1>Not this guy...</h1>
+    <p>But I did.</p>
+    <p>My name is Michael Howe and I created this website.</p>
     <div id="credits">Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
   </div>
 </template>
@@ -8,7 +12,7 @@
 <style >
   #credits {
     position: absolute;
-    bottom: 0;
+    top: 55px;
     font-size: 10px;
   }
 </style>
